f14e4f83606521e71fd180db2eb538becd265cb5
Devangana16 <devanganasuresh70@gmail.com>
2026-03-12 00:43:36 +0530

    Second update

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: "https://log-analyzer-backend.onrender.com/analyze",
 });
 
 api.interceptors.request.use((config) => {
